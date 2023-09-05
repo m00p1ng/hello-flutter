0b41338869c4074332288948d01ade258655aafd
m00p1ng <mongkonchai4412@gmail.com>
2023-09-05 19:59:34 +0700

    add base

--- habit_tracker/lib/main.dart
+++ habit_tracker/lib/main.dart
@@ -1,7 +1,11 @@
 import 'package:flutter/material.dart';
 import 'package:habit_tracker/constants/app_assets.dart';
+import 'package:habit_tracker/constants/app_colors.dart';
+import 'package:habit_tracker/ui/home/home_page.dart';
+import 'package:habit_tracker/ui/theming/app_theme.dart';
 
 Future<void> main() async {
+  WidgetsFlutterBinding.ensureInitialized();
   await AppAssets.preloadSVGs();
   runApp(const MyApp());
 }
@@ -16,6 +20,10 @@ class MyApp extends StatelessWidget {
       theme: ThemeData(
         fontFamily: 'Helvetica Neue',
       ),
+      home: AppTheme(
+        data: AppThemeData.defaultWithSwatch(AppColors.red),
+        child: HomePage(),
+      )
     );
   }
 }
