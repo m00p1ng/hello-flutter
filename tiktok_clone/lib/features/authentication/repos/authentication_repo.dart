120c4a5e3a8c1529c39ff0a5e065a3860fc611c5
m00p1ng <mongkonchai4412@gmail.com>
2023-09-10 21:19:07 +0700

    add login

--- tiktok_clone/lib/features/authentication/repos/authentication_repo.dart
+++ tiktok_clone/lib/features/authentication/repos/authentication_repo.dart
@@ -6,6 +6,35 @@ class AuthenticationRepository {
 
   bool get isLoggedIn => user != null;
   User? get user => _firebaseAuth.currentUser;
+
+  Stream<User?> authStateChanges() => _firebaseAuth.authStateChanges();
+
+  Future<void> emailSignUp(String email, String password) async {
+    await _firebaseAuth.createUserWithEmailAndPassword(
+      email: email,
+      password: password,
+    );
+  }
+
+  Future<void> signOut() async {
+    await _firebaseAuth.signOut();
+  }
+
+  Future<void> signIn(String email, String password) async {
+    await _firebaseAuth.signInWithEmailAndPassword(
+      email: email,
+      password: password,
+    );
+  }
+
+  Future<void> githubSignIn() async {
+    await _firebaseAuth.signInWithProvider(GithubAuthProvider());
+  }
 }
 
 final authRepo = Provider((ref) => AuthenticationRepository());
+
+final authState = StreamProvider((ref) {
+  final repo = ref.read(authRepo);
+  return repo.authStateChanges();
+});
---
+++ tiktok_clone/lib/features/authentication/view_models/signup_view_model.dart
@@ -0,0 +1,39 @@
+import 'dart:async';
+
+import 'package:flutter/material.dart';
+import 'package:flutter_riverpod/flutter_riverpod.dart';
+import 'package:go_router/go_router.dart';
+import 'package:tiktok_clone/features/authentication/repos/authentication_repo.dart';
+import 'package:tiktok_clone/features/onboarding/interests_screen.dart';
+import 'package:tiktok_clone/utils.dart';
+
+class SignUpViewModel extends AsyncNotifier<void> {
+  late final AuthenticationRepository _authRepo;
+
+  @override
+  FutureOr<void> build() {
+    _authRepo = ref.read(authRepo);
+  }
+
+  Future<void> signUp(BuildContext context) async {
+    state = const AsyncValue.loading();
+    final form = ref.read(signUpForm);
+    state = await AsyncValue.guard(
+      () async => await _authRepo.emailSignUp(
+        form["email"],
+        form["password"],
+      ),
+    );
+    if (state.hasError) {
+      showFirebaseErrorSnack(context, state.error);
+    } else {
+      context.goNamed(InterestsScreen.routeName);
+    }
+  }
+}
+
+final signUpForm = StateProvider((ref) => {});
+
+final signUpProvider = AsyncNotifierProvider<SignUpViewModel, void>(
+  () => SignUpViewModel(),
+);
