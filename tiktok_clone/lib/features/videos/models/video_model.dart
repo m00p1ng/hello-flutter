264be3bd696577766324b5b7f09f6c190bebe00f
m00p1ng <mongkonchai4412@gmail.com>
2023-09-10 21:55:03 +0700

    add upload

--- tiktok_clone/lib/features/videos/models/video_model.dart
+++ tiktok_clone/lib/features/videos/models/video_model.dart
@@ -1,4 +1,37 @@
 class VideoModel {
-  String title;
-  VideoModel({required this.title});
+  final String title;
+  final String description;
+  final String fileUrl;
+  final String thumbnailUrl;
+  final String creatorUid;
+  final String creator;
+  final int likes;
+  final int comments;
+  final int createdAt;
+
+  VideoModel({
+    required this.title,
+    required this.description,
+    required this.fileUrl,
+    required this.thumbnailUrl,
+    required this.creatorUid,
+    required this.likes,
+    required this.comments,
+    required this.createdAt,
+    required this.creator,
+  });
+
+  Map<String, dynamic> toJson() {
+    return {
+      "title": title,
+      "description": description,
+      "fileUrl": fileUrl,
+      "thumbnailUrl": thumbnailUrl,
+      "creatorUid": creatorUid,
+      "likes": likes,
+      "comments": comments,
+      "createdAt": createdAt,
+      "creator": creator,
+    };
+  }
 }
--- tiktok_clone/lib/features/videos/view_models/timeline_view_model.dart
+++ tiktok_clone/lib/features/videos/view_models/timeline_view_model.dart
@@ -9,7 +9,17 @@ class TimelineViewModel extends AsyncNotifier<List<VideoModel>> {
   Future<void> uploadVideo() async {
     state = const AsyncValue.loading();
     await Future.delayed(const Duration(seconds: 2));
-    final newVideo = VideoModel(title: "${DateTime.now()}");
+    final newVideo = VideoModel(
+      title: "${DateTime.now()}",
+      description: '',
+      fileUrl: '',
+      thumbnailUrl: '',
+      creatorUid: '',
+      likes: 0,
+      comments: 0,
+      createdAt: 0,
+      creator: '',
+    );
     _list = [..._list, newVideo];
     state = AsyncValue.data(_list);
   }
